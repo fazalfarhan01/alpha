c1ab6caf8238b557183389419d77b161bcc12794
fazalfarhan01 <45929854+fazalfarhan01@users.noreply.github.com>
2019-01-13 16:40:00 +0530

    Update install_plex.sh

--- install_plex.sh
+++ install_plex.sh
@@ -4,6 +4,7 @@ sudo apt-get install apt-transport-https -y
 wget -O - https://dev2day.de/pms/dev2day-pms.gpg.key | sudo apt-key add -
 echo "deb https://dev2day.de/pms/ jessie main" | sudo tee /etc/apt/sources.list.d/pms.list
 sudo apt-get update
+mkdir /home/pi/PLEX
 sudo apt-get install -t jessie plexmediaserver -y
 echo "change user from 'plex' to 'pi'"
 sleep 5
