56f56a9bd83de86144cbea306afa3584f37bdc00
fazalfarhan01 <45929854+fazalfarhan01@users.noreply.github.com>
2019-01-13 16:23:04 +0530

    Update relays.py

--- relays.py
+++ relays.py
@@ -19,11 +19,11 @@ from Adafruit_IO import Client, Feed, RequestError
 # Set to your Adafruit IO key.
 # Remember, your key is a secret,
 # so make sure not to publish it when you publish this code!
-ADAFRUIT_IO_KEY = '34e4ad43c9ef4cb08402e2a8e44f6912'
+ADAFRUIT_IO_KEY = 'YOUR ADAFRUIT SECRET KEY'
 
 # Set to your Adafruit IO username.
 # (go to https://accounts.adafruit.com to find your username)
-ADAFRUIT_IO_USERNAME = 'farhanf1'
+ADAFRUIT_IO_USERNAME = 'ADAFRUIT USER NAME'
 
 # Create an instance of the REST client.
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
