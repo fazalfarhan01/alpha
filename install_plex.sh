190a3a88a101c45710af7f4a70d5f70f14a30ab7
fazalfarhan01 <45929854+fazalfarhan01@users.noreply.github.com>
2019-01-13 16:26:59 +0530

    Create install_plex.sh

---
+++ install_plex.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install apt-transport-https -y
+wget -O - https://dev2day.de/pms/dev2day-pms.gpg.key | sudo apt-key add -
+echo "deb https://dev2day.de/pms/ jessie main" | sudo tee /etc/apt/sources.list.d/pms.list
+sudo apt-get update
+sudo apt-get install -t jessie plexmediaserver -y
+sudo nano /etc/default/plexmediaserver.prev
+sudo service plexmediaserver restart
