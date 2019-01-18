067f2208216d512236bb25d537a07f4faaa090e4
fazalfarhan01 <45929854+fazalfarhan01@users.noreply.github.com>
2019-01-18 21:24:27 +0530

    Update install_plex.sh

--- install_plex.sh
+++ install_plex.sh
@@ -6,29 +6,7 @@ echo "deb https://dev2day.de/pms/ jessie main" | sudo tee /etc/apt/sources.list.
 sudo apt-get update
 mkdir /home/pi/PLEX
 sudo apt-get install -t jessie plexmediaserver -y
-#echo "changing user from 'plex' to 'pi'"
-#sleep 3
+echo "changing user from 'plex' to 'pi'"
+sleep 2
 sudo cp plexmediaserver.prev /etc/default/plexmediaserver.prev
-#
-#
-#sudo rm -r /etc/default/plexmediaserver.prev
-#echo"# default script for Plex Media Server
-
-# the number of plugins that can run at the same time
-#PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS=6
-
-# ulimit -s $PLEX_MEDIA_SERVER_MAX_STACK_SIZE
-#PLEX_MEDIA_SERVER_MAX_STACK_SIZE=3000
-
-# where the mediaserver should store the transcodes
-#PLEX_MEDIA_SERVER_TMPDIR=/tmp
-
-# uncomment to set it to something else
-# PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR="${HOME}/Library/Application\ Support"
-
-# the user that PMS should run as, defaults to 'plex'
-# note that if you change this you might need to move
-# the Application Support directory to not lose your
-# media library
-#PLEX_MEDIA_SERVER_USER=pi" | sudo tee /etc/default/plexmediaserver.prev
 sudo service plexmediaserver restart
