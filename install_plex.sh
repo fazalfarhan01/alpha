6e5dbd9eada0791dd56dc6c6a7f7d3564764e7c5
fazalfarhan01 <45929854+fazalfarhan01@users.noreply.github.com>
2019-01-18 21:20:30 +0530

    Update install_plex.sh

--- install_plex.sh
+++ install_plex.sh
@@ -6,22 +6,22 @@ echo "deb https://dev2day.de/pms/ jessie main" | sudo tee /etc/apt/sources.list.
 sudo apt-get update
 mkdir /home/pi/PLEX
 sudo apt-get install -t jessie plexmediaserver -y
-#echo "change user from 'plex' to 'pi'"
-#sleep 5
-#sudo nano /etc/default/plexmediaserver.prev
+#echo "changing user from 'plex' to 'pi'"
+#sleep 3
+sudo cp plexmediaserver.prev /etc/default/plexmediaserver.prev
 #
 #
-sudo rm -r /etc/default/plexmediaserver.prev
-echo"# default script for Plex Media Server
+#sudo rm -r /etc/default/plexmediaserver.prev
+#echo"# default script for Plex Media Server
 
 # the number of plugins that can run at the same time
-PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS=6
+#PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS=6
 
 # ulimit -s $PLEX_MEDIA_SERVER_MAX_STACK_SIZE
-PLEX_MEDIA_SERVER_MAX_STACK_SIZE=3000
+#PLEX_MEDIA_SERVER_MAX_STACK_SIZE=3000
 
 # where the mediaserver should store the transcodes
-PLEX_MEDIA_SERVER_TMPDIR=/tmp
+#PLEX_MEDIA_SERVER_TMPDIR=/tmp
 
 # uncomment to set it to something else
 # PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR="${HOME}/Library/Application\ Support"
@@ -30,5 +30,5 @@ PLEX_MEDIA_SERVER_TMPDIR=/tmp
 # note that if you change this you might need to move
 # the Application Support directory to not lose your
 # media library
-PLEX_MEDIA_SERVER_USER=pi" | sudo tee /etc/default/plexmediaserver.prev
+#PLEX_MEDIA_SERVER_USER=pi" | sudo tee /etc/default/plexmediaserver.prev
 sudo service plexmediaserver restart
