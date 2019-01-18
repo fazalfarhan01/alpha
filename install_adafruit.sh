7370c7c25c88a23b673d1b4f13e890f325e1e244
fazalfarhan01 <45929854+fazalfarhan01@users.noreply.github.com>
2019-01-18 21:32:18 +0530

    Create install_adafruit.sh

---
+++ install_adafruit.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo pip3 install --upgrade setuptools
+sudo apt-get install RPi.GPIO -y
+pip3 install RPi.GPIO
+pip3 install adafruit-blinka
+pip3 install adafruit-io
+cd ~/Desktop
+git clone https://github.com/adafruit/io-client-python.git
+git clone https://github.com/fazalfarhan01/autohome.git
