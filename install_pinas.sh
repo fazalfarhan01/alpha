50ed3996b7b295b9771ad88ab5226d2f405f789c
fazalfarhan01 <45929854+fazalfarhan01@users.noreply.github.com>
2019-02-14 16:28:30 +0530

    Update install_pinas.sh

--- install_pinas.sh
+++ install_pinas.sh
@@ -1,16 +1,17 @@
 sudo apt-get update
 sudo apt-get install samba samba-common-bin -y
 sudo rm -r /etc/samba/smb.conf
-echo "[HOME]
-comment = HOME
-browseable = yes
-path = /home/pi
-writeable = Yes
-create mask = 0777
-directory mask = 0777
-browseable = Yes
-public = yes
-"| sudo tee /etc/samba/smb.conf
+#echo "[HOME]
+#comment = HOME
+#browseable = yes
+#path = /home/pi
+#writeable = Yes
+#create mask = 0777
+#directory mask = 0777
+#browseable = Yes
+#public = yes
+#"| sudo tee /etc/samba/smb.conf
+cp smb.conf /etc/samba/smb.conf
 echo "Enter a Password for NAS"
 sudo smbpasswd -a pi
 sudo /etc/init.d/samba restart
