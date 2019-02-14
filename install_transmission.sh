80c1060658ee1ebc3329e6d9965a8957a106bd14
fazalfarhan01 <45929854+fazalfarhan01@users.noreply.github.com>
2019-02-14 17:02:19 +0530

    Update install_transmission.sh

--- install_transmission.sh
+++ install_transmission.sh
@@ -2,6 +2,6 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install transmission-daemon -y
 sudo systemctl stop transmission-daemon
-cp settings.json /etc/transmission-daemon/settings.json
+sudo cp settings.json /etc/transmission-daemon/settings.json
 sudo ufw allow 9091,51413/tcp
 sudo systemctl start transmission-daemon
